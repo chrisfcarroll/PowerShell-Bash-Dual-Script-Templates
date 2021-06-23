54642e70cb6fc547a8257a5b41ebb08b03dd451f
Chris F Carroll <chris.carroll@unforgettable.me.uk>
2021-06-23 16:06:36 +0100

    powershell-everywhere.ps1 for powershell everywhere

---
+++ powershell-everywhere.ps1
@@ -0,0 +1,4 @@
+#! /usr/bin/env pwsh
+# The one-liner above marks this for unix shells as runnable in powershell. 
+# But: you still have to chmod it to be executable from a bash or fish or other shell
+$PSVersionTable
